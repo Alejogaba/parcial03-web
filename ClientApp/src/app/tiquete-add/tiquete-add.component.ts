9af6f2230b59d6eb1aad9d8b6c923b7819cb5025
Diego Garces <alejogaba_1116@hotmail.com>
2019-12-10 14:45:04 -0500

    .

--- ClientApp/src/app/tiquete-add/tiquete-add.component.ts
+++ ClientApp/src/app/tiquete-add/tiquete-add.component.ts
@@ -1,4 +1,12 @@
 import { Component, OnInit } from '@angular/core';
+import { Tiquete } from '../models/tiquete';
+import { Cliente } from '../models/cliente';
+import { Ruta } from '../models/ruta';
+import { ClienteDataService } from '../services/cliente-data.service';
+import { TiqueteDataService } from '../services/tiquete-data.service';
+import { RutaDataService } from '../services/ruta-data.service';
+
+
 
 @Component({
   selector: 'app-tiquete-add',
@@ -6,10 +14,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./tiquete-add.component.css']
 })
 export class TiqueteAddComponent implements OnInit {
-
-  constructor() { }
-
+rutas:Ruta[];
+tiquete:Tiquete;
+clientes:Cliente[];
+  constructor(private rutaservice:RutaDataService) { }
   ngOnInit() {
+    this.getRutas();
   }
-
+  getRutas() {
+    this.rutaservice.get().subscribe(c => {
+      return this.rutas = c;
+    });
+    }
 }
